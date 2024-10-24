3c7c5665cbbdcd5a7edd9054c890dfc95aa6c19b
Vinicius <vinicius.padua@tarvos.ag>
2024-10-24 00:06:48 -0300

    feat: configure setup of app

---
+++ node-api/src/main/config/app.ts
@@ -0,0 +1,13 @@
+import setupMiddlewares from "@/main/config/middlewares";
+import setupRoutes from "@/main/config/routes";
+import setupSwagger from "@/main/config/swagger";
+
+import express, { Express } from "express";
+
+export const setupApp = async (): Promise<Express> => {
+  const app = express();
+  setupSwagger(app);
+  setupMiddlewares(app);
+  setupRoutes(app);
+  return app;
+};
