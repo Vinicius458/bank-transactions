44ee22d062bed3c1cbc7136e853d597c9df8dd41
Vinicius <vinicius.padua@tarvos.ag>
2024-10-24 20:56:02 -0300

    feat: refactor nomenclature domain with-draw usecase

--- node-api/src/data/usecases/deposit.ts
+++ node-api/src/data/usecases/deposit.ts
@@ -2,15 +2,15 @@ import { Deposit } from "@/domain/usecases";
 import { AccountRepository } from "@/data/protocols/db";
 
 export class DepositUseCase implements Deposit {
-  constructor(private data: AccountRepository) {}
+  constructor(private accountRepo: AccountRepository) {}
 
-  async execute(deposit: Deposit.Params): Promise<void> {
-    const account = await this.data.findById(deposit.accountId);
+  async execute(data: Deposit.Params): Promise<void> {
+    const account = await this.accountRepo.findById(data.accountId);
     if (!account) throw new Error("Conta não encontrada");
 
-    account.credit(deposit.amount);
+    account.credit(data.amount);
 
-    const success = await this.data.updateAccount(account);
+    const success = await this.accountRepo.updateAccount(account);
     if (!success)
       throw new Error(
         "Erro ao atualizar a conta, possivelmente devido a um conflito de versão"
