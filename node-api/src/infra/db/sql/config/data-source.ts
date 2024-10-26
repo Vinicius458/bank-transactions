c27ab17ad3fcd6b27a4486981af8ea505bb7b674
Vinicius <vinicius.padua@tarvos.ag>
2024-10-25 22:06:50 -0300

    feat: new Datasource initilize

--- node-api/src/infra/db/sql/config/data-source.ts
+++ node-api/src/infra/db/sql/config/data-source.ts
@@ -1,14 +1,22 @@
+import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { Account } from "../account/account.entity";
+import { Transaction } from "../transaction/transaction.entity";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: "localhost",
   port: 3306,
   username: "root",
-  password: "root",
-  database: "bank2142",
-  synchronize: true,
-  logging: false,
-  entities: [],
-  subscribers: [],
+  password: "123",
+  database: "bank", // banco de dados para teste
+  entities: [Account, Transaction],
+  synchronize: true, // cria as tabelas automaticamente no banco de teste
 });
+
+export const initializeTestDataSource = async () => {
+  if (!AppDataSource.isInitialized) {
+    await AppDataSource.initialize();
+  }
+  return AppDataSource;
+};
